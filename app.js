ca7e501964f490e48ddd7eb54f0fe2c2931119fc
prajita11 <timprajita100@gmail.com>
2024-08-16 19:59:12 +0545

    add values in postman

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const connectToDatabase = require('./database')
 
 const app =express()
+app.use(express.json())
 
 connectToDatabase()
 
@@ -12,12 +13,18 @@ app.get("/",(req ,res)=>{
     })
 })
 
-app.get("/about" ,(req, res) =>{
-    res.json({
-        message : "This is about page"
-    })
-})
+// app.get("/about" ,(req, res) =>{
+//     res.json({
+//         message : "This is about page"
+//     })
+// })
 
+app.post("/blog" ,(req,res) =>{
+    console.log(req.body)
+    res.status(200).json({
+    message: "Blog api hit successfully"
+ })
+})
 
 app.listen(process.env.PORT,()=>
     console.log("NodeJs project has started")
