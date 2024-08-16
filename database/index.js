ee5861c3059197a22caaf9b66d31ebabcc02ac2d
prajita11 <timprajita100@gmail.com>
2024-08-16 18:26:08 +0545

    dotenv

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const mongoose= require('mongoose')
 
 function connectToDatabase(){
-    mongoose.connect('')
+    mongoose.connect('process.env.MONGODB_URI')
     console.log("Database connected successfully")
 }
 
